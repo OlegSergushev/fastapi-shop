ec0e312c94c641d630fc7541c54065df544b0b25
Oleg Sergushev <osergushev@gmail.com>
2025-10-22 11:45:28 +0300

    Add files via upload

---
+++ app/models/reviews.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import String, Integer, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment: Mapped[str] = mapped_column(String, nullable=True)
+    comment_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    grade: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+
